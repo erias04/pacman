8cc0704d01c17267d14c23bd810a8b06bcfcb470
erias04 <lichtbargeld@gmail.com>
2021-10-03 17:50:38 +0200

    import start-button from index.html

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ import { LEVEL, OBJECT_TYPE } from './setup';
 // DOM Elements from index.html
 const gameGrid = document.querySelector('#gameGrid');
 const scoreTable = document.querySelector('#score');
+const startButton = document.querySelector('#start-button');
