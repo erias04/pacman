f9d69425d57ca0e617c7ac7ccfc10583b74c4f7f
erias04 <lichtbargeld@gmail.com>
2021-10-03 18:08:06 +0200

    added new constants with this.{constant} for GameBoard.js

--- GameBoard.js
+++ GameBoard.js
@@ -2,6 +2,8 @@ import { GRID_SIZE, CELL_SIZE, OBJECT_TYPE, CLASS_LIST } from './setup'
 
 class GameBoard {
     constructor(DOMGrid) {
-
-    } 
+        this.dotCount = 0;
+        this.grid = [];
+        this.DOMGrid = DOMGrid;
+    }
 }
