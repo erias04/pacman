f088c1573b9e7558601aef6aaec3141d68ae0169
erias04 <lichtbargeld@gmail.com>
2021-12-21 16:43:09 +0100

    feat: import pacman image

--- app/scripts/characters/pacman.js
+++ app/scripts/characters/pacman.js
@@ -11,6 +11,16 @@ export default class Pacman {
     draw(ctx) {}
 
     #loadPacmanImages() {
-        
+        const pacmanImage1 = new Image();
+        pacmanImage1.src ='/app/style/graphics/pac0.png'
+
+        const pacmanImage2 = new Image();
+        pacmanImage2.src ='/app/style/graphics/pac1.png'
+
+        const pacmanImage3 = new Image();
+        pacmanImage3.src ='/app/style/graphics/pac2.png'
+
+        const pacmanImage4 = new Image();
+        pacmanImage4.src ='/app/style/graphics/pac1.png'
     }
 }  
