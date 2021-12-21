64c40d4d6a7efaef3b334e5424dde2c4bbb53f1b
erias04 <lichtbargeld@gmail.com>
2021-12-21 12:14:39 +0100

    feat: commend content in GameLoop

--- app/scripts/core/Game.js
+++ app/scripts/core/Game.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
 
 function gameLoop() {
-    console.log(75);
+    // console.log(75);
 }
 
 setInterval(gameLoop, 1000 / 75)
--- build/app.js
+++ build/app.js
@@ -1,11 +1,19 @@
-
-
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
 
 function gameLoop() {
-    console.log(75);
+    // console.log(75);
 }
 
 setInterval(gameLoop, 1000 / 75)
+
+export default class TileMap{
+    constructor(tileSize) {
+        this.tileSize = tileSize;
+    }
+
+    draw() {
+        console.log('draw')
+    }
+}
 
