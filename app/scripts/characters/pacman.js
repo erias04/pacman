0893a4fb5cdef5891d89cf28cd694d787fc4289b
erias04 <lichtbargeld@gmail.com>
2021-12-21 21:35:39 +0100

    feat: rotate pacman when moving

--- app/scripts/characters/pacman.js
+++ app/scripts/characters/pacman.js
@@ -15,17 +15,30 @@ export default class Pacman {
         this.pacmanAnimationTimerDefault = 10;
         this.pacmanAnimationTimer = null;
 
+        this.pacmanRotation = this.Rotation.right;
+
         this.#loadPacmanImages();
     }
 
     Rotation = {
-        
+        right: 0,
+        down: 1,
+        left: 2,
+        up: 3
     }
     
     draw(ctx) {
         this.#move();
         this.#animate();
-        ctx.drawImage(this.pacmanImages[this.pacmanImageIndex], this.x, this.y, this.tileSize, this.tileSize)
+        const size = this.tileSize / 2;
+
+        ctx.save();
+        ctx.translate(this.x + size, this.y + size);
+        ctx.rotate((this.pacmanRotation * 90 * Math.PI) / 180);
+        ctx.drawImage(this.pacmanImages[this.pacmanImageIndex], -size, -size, this.tileSize, this.tileSize);
+
+        ctx.restore();
+        // ctx.drawImage(this.pacmanImages[this.pacmanImageIndex], this.x, this.y, this.tileSize, this.tileSize)
     }
 
     #loadPacmanImages() {
@@ -95,18 +108,22 @@ export default class Pacman {
         switch(this.currentMovingDirection) {
             case MovingDirection.up:
                 this.y -= this.velocity;
+                this.pacmanRotation = this.Rotation.up;
                 break;
 
             case MovingDirection.down:
                 this.y += this.velocity;
+                this.pacmanRotation = this.Rotation.down;
                 break;
 
             case MovingDirection.right:
                 this.x += this.velocity;
+                this.pacmanRotation = this.Rotation.right;
                 break;
 
             case MovingDirection.left:
                 this.x -= this.velocity;
+                this.pacmanRotation = this.Rotation.left;
                 break;
         }
     }
